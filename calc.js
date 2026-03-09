614cc9701d03013a8b17f5399acc4648acd15c7f
K S Keerthana <ksk66117@gmail.com>
2026-03-09 21:39:50 +0530

    Added HTML, CSS, JavaScript files for calculator project
    Basic calculator with arithmetic operations, keyword support, and responsive UI

---
+++ calc.js
@@ -0,0 +1,44 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+display.value += value;
+}
+
+function clearDisplay(){
+display.value = "";
+}
+
+function deleteLast(){
+display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+try{
+display.value = eval(display.value);
+}
+catch{
+display.value = "Error";
+}
+}
+
+/* Keyboard Support */
+
+document.addEventListener("keydown",function(event){
+
+if(!isNaN(event.key) || "+-*/.".includes(event.key)){
+display.value += event.key;
+}
+
+if(event.key === "Enter"){
+calculate();
+}
+
+if(event.key === "Backspace"){
+deleteLast();
+}
+
+if(event.key === "Escape"){
+clearDisplay();
+}
+
+});
